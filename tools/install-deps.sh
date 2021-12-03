49a020f82e13070b687175ac9e2c2c1ce89dfd7e
iphydf <iphydf@users.noreply.github.com>
2021-12-03 02:46:47 +0000

    chore: Require CI to pass before merging PRs.

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -2,31 +2,15 @@
 
 set -eux
 
-# install libsodium, needed for crypto
-if ! [ -d libsodium ]; then
-  git clone --depth=1 --branch=1.0.3 https://github.com/jedisct1/libsodium.git
-fi
-cd libsodium
-git rev-parse HEAD > libsodium.sha
-if ! ([ -f "$CACHE_DIR/libsodium.sha" ] && diff "$CACHE_DIR/libsodium.sha" libsodium.sha); then
-  ./autogen.sh
-  ./configure --prefix="$CACHE_DIR/usr"
-  make -j`nproc`
-  make install
-  mv libsodium.sha "$CACHE_DIR/libsodium.sha"
-fi
-cd ..
-rm -rf libsodium
-
 # install toxcore
 if ! [ -d toxcore ]; then
   git clone --depth=1 --branch=master https://github.com/TokTok/toxcore.git toxcore
 fi
 cd toxcore
-git rev-parse HEAD > toxcore.sha
+git rev-parse HEAD >toxcore.sha
 if ! ([ -f "$CACHE_DIR/toxcore.sha" ] && diff "$CACHE_DIR/toxcore.sha" toxcore.sha); then
-  cmake -B_build -H. -DCMAKE_INSTALL_PREFIX:PATH=$HOME/cache/usr
-  make -C_build -j`nproc`
+  cmake -B_build -H. -DCMAKE_INSTALL_PREFIX:PATH="$HOME/cache/usr"
+  make -C_build -j"$(nproc)"
   make -C_build install
   mv toxcore.sha "$CACHE_DIR/toxcore.sha"
 fi
